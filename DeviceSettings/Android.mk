6e4395efe9a90b09c17da57541ba55d13c4816cd
TheWolf91 <the_wolf91@libero.it>
2014-03-16 14:13:57 +0100

    Revert "Aries parts from Mackay-Omni"
    This reverts commit b4bfdb335c64951d860009d2d24feafcccbec3ac.

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -1,3 +1,21 @@
+#
+# Copyright (C) The Android Open Source Project
+# Copyright (C) The CyanogenMod Project
+# Copyright (C) The OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -7,7 +25,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := AriesParts
+LOCAL_PACKAGE_NAME := DeviceSettings
 LOCAL_CERTIFICATE := platform
 
 # Required so that symbols used by the jni library doesn't get stripped out
