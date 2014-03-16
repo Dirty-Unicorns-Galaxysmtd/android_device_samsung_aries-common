b4bfdb335c64951d860009d2d24feafcccbec3ac
TheWolf91 <the_wolf91@libero.it>
2014-03-16 14:12:18 +0100

    Aries parts from Mackay-Omni

---
+++ DeviceSettings/src/org/omnirom/omnigears/device/Startup.java
@@ -0,0 +1,34 @@
+package org.omnirom.device;
+
+import android.content.BroadcastReceiver;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+        Apply.restore(context);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        int apply = sharedPrefs.getBoolean(DeviceSettings.KEY_APPLY, true) ? 1 : 0;
+	if(apply == 1) {
+        ColorTuningPreference.restore(context);
+        Mdnie.restore(context);
+        TouchKeyBacklightTimeout.restore(context);
+        Led.restore(context);
+        Hspa.restore(context);
+        VolumeBoostPreference.restore(context);
+        DockAudio.restore(context);
+        Sanity.check(context);
+        VibrationPreference.restore(context);
+        Wifi.restore(context);
+        BLXPreference.restore(context);
+        Navigation.restore(context);
+        TouchWake.restore(context);
+        TouchWakePreference.restore(context);
+	}
+    }
+
+}
